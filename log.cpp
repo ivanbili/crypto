41a6a62683eb160fae62571b8fdc085f4eb15ea6
ivanbili <ivanbili@gmail.com>
2021-03-04 23:01:16 -0500

    Update log.cpp

--- log.cpp
+++ log.cpp
@@ -6,7 +6,7 @@
 
 int main()
    {
-	 std::unordered_map<std::string, unsigned long int> map;
+   std::unordered_map<std::string, unsigned long int> map;
    mpz_t p, g, g_pwr, h, g_inv, g_pwr_b, left, right;
    mpz_init_set_str (p, "13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171", 10);
    mpz_init_set_str (g, "11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568", 10);
@@ -17,7 +17,7 @@ int main()
    map[mpz_get_str (NULL, 10, left)] = 0;
    for (unsigned long int i = 1; i <= 1048576; i++) // 2^20 = 1048576
       {
-      mpz_mul (left, left, g_inv); // g_pwr = g^i
+      mpz_mul (left, left, g_inv);
       mpz_mod (left, left, p);
       map[mpz_get_str (NULL, 10, left)] = i;
       }   
@@ -33,7 +33,7 @@ int main()
    mpz_mod (g_pwr_b, g_pwr_b, p);
    for (unsigned long int i = 1; i <= 1048576; i++)
       {
-      mpz_mul (right, right, g_pwr_b); // g_pwr = g^i
+      mpz_mul (right, right, g_pwr_b);
       mpz_mod (right, right, p);
       auto got = map.find (mpz_get_str (NULL, 10, right));
       if (got != map.end())
@@ -42,6 +42,6 @@ int main()
          return 0;
       	 }
       }
-   std::cout << "Solution not found :(" << std::endl; //x0 is zero
+   std::cout << "Solution not found :(" << std::endl;
    return 0;
    }
